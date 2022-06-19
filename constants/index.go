46912d94041194651e133edc5c56f0a7d6de3e4d
Manuel Luna <manuellunada@gmail.com>
2022-06-19 18:38:56 -0500

    term info

--- constants/index.go
+++ constants/index.go
@@ -4,11 +4,8 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
-	"os"
 	"path/filepath"
 	"runtime"
-
-	"golang.org/x/term"
 )
 
 var _, base, _, ok = runtime.Caller(0)
@@ -19,10 +16,6 @@ const (
 	PrimaryDark = "#757a2d"
 	Secondary   = "#000000"
 	Dark        = "#4a4848"
-)
-
-var (
-	TermWidth, TermHeight, _ = term.GetSize(int(os.Stdout.Fd()))
 )
 
 func RetrieveTokenWithoutCheck() string {
