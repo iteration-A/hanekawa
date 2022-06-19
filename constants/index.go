4509b376e59ca5356f3427729b4edfc95771986d
Manuel Luna <manuellunada@gmail.com>
2022-06-19 18:39:46 -0500

    paths

--- constants/index.go
+++ constants/index.go
@@ -4,12 +4,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
-	"path/filepath"
-	"runtime"
 )
-
-var _, base, _, ok = runtime.Caller(0)
-var Basepath = filepath.Join(filepath.Dir(base), "../")
 
 func RetrieveTokenWithoutCheck() string {
 	bytes, err := ioutil.ReadFile(fmt.Sprintf("%s/.token", Basepath))
