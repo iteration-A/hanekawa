8d8db9c19c3c0226a4155bc2bd8b3dbc926f1ec2
Manuel Luna <manuellunada@gmail.com>
2022-07-05 21:31:06 -0500

    set new banner

--- login/ascii.go
+++ login/ascii.go
@@ -10,12 +10,14 @@ import (
 
 var art string
 
+const ASCII = "rice"
+
 func Art() string {
 	if art != "" {
 		return art
 	}
 
-	content, err := ioutil.ReadFile(fmt.Sprintf("%s/ascii/hanekawa", constants.Basepath))
+	content, err := ioutil.ReadFile(fmt.Sprintf("%s/ascii/%s", constants.Basepath, ASCII))
 
 	if err != nil {
 		log.Fatal("Could not read ascii art", err)
